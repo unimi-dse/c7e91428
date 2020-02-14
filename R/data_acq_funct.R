0c2c6dd3557287839a0f65e2560aceb706bf5f56
pietromatteo <60604371+pietromatteo@users.noreply.github.com>
2020-02-14 13:32:44 +0100

    new data acq

--- R/data_acq_funct.R
+++ R/data_acq_funct.R
@@ -1,6 +1,6 @@
 data_acq()<-function(){
-  datasetmrct<-read.csv(system.file("extdata", "marcatoriseriea1", package="ciccio"),sep=",")
-  rownames(datasetmrct)<-datasetmrct[[1]]
-  datasetmrct[1]<-NULL
-  return(datasetmrct)
+  dataset<-read.csv(system.file("extdata", "marcatoriseriea1", package="ciccio"),sep=",")
+  rownames(dataset)<-dataset[[1]]
+  dataset[1]<-NULL
+  return(dataset)
 }
