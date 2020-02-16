1d64fa303371b1fcb4a3b0f3f247340ea5a46f24
pietromatteo <60604371+pietromatteo@users.noreply.github.com>
2020-02-16 15:45:00 +0100

    upload dataacq

--- R/data_acq_funct.R
+++ R/data_acq_funct.R
@@ -12,6 +12,6 @@
 dataacq()<-function(){
   dataset<-read.csv(system.file("extdata", "marcatoriseriea1", package="ciccio"),sep=",")
   rownames(dataset)<-dataset[[1]]
-  dataset[1]<-NULL
+  dataset[[1]]<-NULL
   return(dataset)
 }
