806746a286619c4aedbc4491f75e573eded69bf0
pietromatteo <60604371+pietromatteo@users.noreply.github.com>
2020-02-16 16:38:19 +0100

    upload

--- R/getdata_funct.R
+++ R/getdata_funct.R
@@ -1,16 +1,5 @@
-# @description this function imports the dataset and assign the first column as rownames of the dataset
-# Run this function to acquire the dataset.
-#
-# @return dataframe
-#
-# @examples
-# getdata()
-#
-# @export
-#
-
-getdata()<-function(){
-  dataset<-read.csv(system.file("extdata", "marcatoriseriea1", package="ciccio"),sep=",")
+getdata<-function(){
+  dataset<-read.csv(system.file("extdata", "marcatoriseriea1.csv", package="ciccio"),sep=",")
   rownames(dataset)<-dataset[[1]]
   return(dataset)
 }
