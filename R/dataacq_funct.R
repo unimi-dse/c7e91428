0f1df9d3657e086a8ae7d2dfb4d139cb31079c0a
pietromatteo <60604371+pietromatteo@users.noreply.github.com>
2020-02-16 15:57:45 +0100

    hi

--- R/dataacq_funct.R
+++ R/dataacq_funct.R
@@ -1,3 +1,5 @@
+# @description this function imports the dataset and assign the first column as rownames of the dataset
+# Run this function to acquire the dataset.
 #
 # @return dataframe
 #
@@ -10,3 +12,6 @@
 dataacq()<-function(){
   dataset<-read.csv(system.file("extdata", "marcatoriseriea1", package="ciccio"),sep=",")
   rownames(dataset)<-dataset[[1]]
+  dataset[[1]]<-NULL
+  return(dataset)
+}
--- R/scoreplot_funct.R
+++ R/scoreplot_funct.R
@@ -1,4 +1,4 @@
-plottare<-function(){
+scoreplot<-function(){
 ggplot2::ggplot(dataset, ggplot2::aes(x=season,))+
 ggplot2::geom_point(ggplot2::aes(y=BELOTTI, col="green"))+
 ggplot2::geom_point(ggplot2::aes(y=ZAPATA, col="red"))+
