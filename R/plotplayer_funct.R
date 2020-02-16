22571928682f750488ea4f1558c2b09431ac33c4
pietromatteo <60604371+pietromatteo@users.noreply.github.com>
2020-02-16 23:58:59 +0100

    ma

--- R/plotplayer_funct.R
+++ R/plotplayer_funct.R
@@ -1,6 +1,6 @@
 #' create a barplot with values
 #' @description this function enables to visualize graphically the performace of a single player in the season considered in the dataset
-#' @param id character. the name of the player you're interested in
+#'
 #' @return barplot
 #' x axis default season
 #' y axis playername selected as param
@@ -11,8 +11,14 @@
 #'
 
 
-plotplayer<-function(p){
-  p<-toupper(p)
-  ggplot2::ggplot(dataset,ggplot2::aes(x=season,y=dataset[[p]]))+
+plotplayer<-function(dataset=NULL){
+  if (is.null(dataset)==TRUE)
+    dataset<-newdataset()
+  else (dataset<-dataset)
+  ggplot2::ggplot(dataset,ggplot2::aes(x=season, y=BELOTTI)+
   ggplot2::geom_col()
 }
+
+
+
+
--- R/scoreplot_funct.R
+++
@@ -1,9 +0,0 @@
-scoreplot<-function(){
-ggplot2::ggplot(dataset, ggplot2::aes(x=season,))+
-ggplot2::geom_point(ggplot2::aes(y=BELOTTI, col="green"))+
-ggplot2::geom_point(ggplot2::aes(y=ZAPATA, col="red"))+
-ggplot2::geom_point(ggplot2::aes(y=HIGUAIN, col="blue"))+
-ggplot2::geom_point(ggplot2::aes(y=MERTENS, col="yellow"))
-}
-
-
