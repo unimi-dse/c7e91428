ba71c69d4f59721886fd4fd36d58bb9050a2e7dd
pietromatteo <60604371+pietromatteo@users.noreply.github.com>
2020-02-16 16:12:31 +0100

    upload funct

--- R/getdata_funct.R
+++ R/getdata_funct.R
@@ -4,14 +4,13 @@
 # @return dataframe
 #
 # @examples
-# dataacq()
+# getdata()
 #
 # @export
 #
 
-dataacq()<-function(){
+getdata()<-function(){
   dataset<-read.csv(system.file("extdata", "marcatoriseriea1", package="ciccio"),sep=",")
   rownames(dataset)<-dataset[[1]]
-  dataset[[1]]<-NULL
   return(dataset)
 }
